180016cac7438cf8cdc5f6d036c53bd3add163a8
Fausto Busso <faustobusso@gmail.com>
2024-09-10 01:15:29 -0300

    Class 3 level
    nuevas escenas para cuando perdes o ganas, puntuacion y monedas, y zona de muerte

--- Assets/Scripts/RigibogyMovement.cs
+++ Assets/Scripts/RigibogyMovement.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RigibogyMovement : MonoBehaviour
 {
     public float speed;
     public Vector2 inputVector;
     public Rigidbody rigidBody;
+    public Vector3 velocity;
+    public float velocityMagnitude;
     public float jumpForce;
+    public bool canJump;
+
+    public int collectedItems;
+
+    public TMPro.TextMeshProUGUI scoreText;
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        canJump = true;
     }
 
     
@@ -21,9 +32,45 @@ public class RigibogyMovement : MonoBehaviour
 
         rigidBody.AddForce(inputVector.x * speed, 0f, inputVector.y * speed, ForceMode.Impulse);
 
-        if(Input.GetKeyDown(KeyCode.Space)) 
+        velocity = rigidBody.velocity;
+        velocityMagnitude = velocity.magnitude;
+
+        if(Input.GetKeyDown(KeyCode.Space) && canJump) 
         {
             rigidBody.AddForce(0f, jumpForce, 0f, ForceMode.Impulse);
+            canJump = false;
         }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Choque contra: " + collision.gameObject.name);
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            canJump = true;
+        }
+
+        if (collision.gameObject.CompareTag("KillZone"))
+        {
+            Debug.Log ("KILL MEEEE");
+
+            SceneManager.LoadScene(0);
+        }
+
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("Ganaste, felicitaciones");
+
+            SceneManager.LoadScene(1);
+        }
+
+        if (collision.gameObject.CompareTag("Coin"))
+        {
+            Destroy(collision.gameObject);
+            collectedItems++;
+            scoreText.text = collectedItems.ToString();
+        }
+    }
+
 }
